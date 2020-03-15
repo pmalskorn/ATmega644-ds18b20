1143af90d14d8644804d42dcd85eb61ab97c1aad
Unknown <philipp.malskorn@rwth-aachen.de>
2020-03-16 00:01:42 +0100

    push project Files

---
+++ ds18b20/ds18b20/ds18b20.c
@@ -0,0 +1,88 @@
+#include <inttypes.h>
+#include <util/crc16.h>
+#include <util/delay.h>
+#include "OneWire.h"
+#include "ds18.h"
+
+
+// Helper Functions
+uint8_t ds18b20_checkcrc(uint8_t data[], uint8_t length)
+{
+	uint8_t crc = 0;
+	for (uint8_t i = 0; i < length; i++){
+		crc = _crc_ibutton_update(crc, data[i]);
+	}
+	return crc;
+}
+
+
+uint8_t ds18b20_crc_scratchpad(scratchpad_t *sp)
+{
+	uint8_t data[] = {
+		sp->temp_lsb,
+		sp->temp_msb,
+		sp->th,
+		sp->tl,
+		sp->config,
+		sp->reserved1,
+		sp->reserved2,
+		sp->reserved3,
+		sp->crc
+	};
+	return ds18b20_checkcrc(data,9);
+}
+
+uint8_t ds18b20_crc_rom(rom_t rom)
+{
+	uint8_t data[8];
+	for (uint8_t i = 0; i<8;i++)
+	{
+		data[i] = rom >> 8*i;
+	}
+	return ds18b20_checkcrc(data,8);
+}
+
+// ROM Commmands
+rom_t ds18b20_read_rom(onewire_t *ow)
+{
+	onewire_reset(ow);
+	onewire_write_byte(ow, READ_ROM);
+	
+	rom_t rom = 0;
+	for(uint8_t i = 0; i < 8; i++){
+		volatile uint8_t byte = onewire_read_byte(ow);
+		rom |= ((uint64_t)byte) << 8*i; 
+	}
+
+	return rom;
+}
+
+void ds18b20_skip_rom(onewire_t *ow){
+	onewire_reset(ow);
+	onewire_write_byte(ow, SKIP_ROM);
+}
+
+// Function Commands
+void ds18b20_convert_t(onewire_t *ow){
+
+	onewire_write_byte(ow, COVERT_T);
+	while(onewire_read_bit(ow));
+}
+
+scratchpad_t ds18b20_read_scratchpad(onewire_t *ow){
+
+	onewire_write_byte(ow, READ_SCRATCHPAD);
+	scratchpad_t sp;
+
+	sp.temp_lsb = onewire_read_byte(ow);
+	sp.temp_msb = onewire_read_byte(ow);
+	sp.th = onewire_read_byte(ow);
+	sp.tl = onewire_read_byte(ow);
+	sp.config = onewire_read_byte(ow);
+	sp.reserved1 = onewire_read_byte(ow);
+	sp.reserved2 = onewire_read_byte(ow);
+	sp.reserved3 = onewire_read_byte(ow);
+	sp.crc = onewire_read_byte(ow);
+	
+	return sp;
+}
---
+++ ds18b20/ds18b20/ds18b20.h
@@ -0,0 +1,62 @@
+#ifndef DS18_H_
+#define DS18_H_
+
+#include "OneWire.h"
+
+typedef uint64_t rom_t;
+
+typedef struct scratchpad_t
+{
+	volatile uint8_t temp_lsb;
+	volatile uint8_t temp_msb;
+	volatile uint8_t th;
+	volatile uint8_t tl;
+	volatile uint8_t config;
+	volatile uint8_t reserved1;
+	volatile uint8_t reserved2;
+	volatile uint8_t reserved3;
+	volatile uint8_t crc;
+} scratchpad_t;
+
+// ROM Commmands
+#define SEARCH_ROM		0xF0
+#define READ_ROM		0x33
+#define MATCH_ROM		0x55
+#define SKIP_ROM		0xCC
+#define ALARM_SEARCH	0xEC
+
+// Function Commmands
+#define COVERT_T			0x44
+#define WRITE_SCRATCHPAD	0x4E
+#define READ_SCRATCHPAD		0xBE
+#define COPY_SCRATCHPAD		0x48
+#define RECALL_E2			0xB8
+#define READ_POWER_SUPPLY	0xB4
+
+// Helper Functions
+uint8_t ds18b20_checkcrc(uint8_t data[], uint8_t length);
+uint8_t ds18b20_crc_scratchpad(scratchpad_t *sp);
+uint8_t ds18b20_crc_rom	(rom_t rom);
+//uint8_t ds18b20_
+
+
+// ROM Commmands
+void	ds18b20_search_rom(onewire_t *ow);
+rom_t	ds18b20_read_rom(onewire_t *ow);
+void	ds18b20_match_rom(onewire_t *ow);
+void	ds18b20_skip_rom(onewire_t *ow);
+void	ds18b20_alarm_search(onewire_t *ow);
+
+
+// Function Commands
+void			ds18b20_convert_t(onewire_t *ow);
+void			ds18b20_write_scratchpad(onewire_t *ow);
+scratchpad_t	ds18b20_read_scratchpad(onewire_t *ow);
+void			ds18b20_copy_scratchpad(onewire_t *ow);
+void			ds18b20_recall_eeprom(onewire_t *ow);
+void			ds18b20_read_power_supply(onewire_t *ow);
+
+
+
+
+#endif /* DS18_H_ */
